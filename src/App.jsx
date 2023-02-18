638f0fc570c49b1e5eeac2ed2ac035aee3054f44
Santiago Ruiz <103048072+Santi1994r@users.noreply.github.com>
2023-02-18 19:39:52 -0300

    create temp component (#2)
    Co-authored-by: Brajhan Lopez <102929742+BrajhanLop@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import Temp from "./components/Temp/Temp"
 import CardNextDays from "./components/CardNextDays/CardNextDays"
+
 
 function App() {
  
 
   return (
     <div>
-        
+      <Temp />
     </div>
   )
 }
